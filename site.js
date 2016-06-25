ef5423804390a037c3946645c1b8d5d14a921f8f
sara4844 <sraleroy@gmail.com>
2016-06-25 14:14:09 -0400

    added moco bikelanes

--- site.js
+++ site.js
@@ -1,18 +1,22 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoiYWx1bHNoIiwiYSI6ImY0NDBjYTQ1NjU4OGJmMDFiMWQ1Y2RmYjRlMGI1ZjIzIn0.pngboKEPsfuC4j54XDT3VA';
 
 var map = L.mapbox.map('map', 'mapbox.light', { zoomControl: false })
-            .setView([38.898, -77.043], 12);
+    .setView([38.898, -77.043], 12);
 
 new L.Control.Zoom({ position: 'bottomright' }).addTo(map);
 
 var bufferLayer = L.mapbox.featureLayer().addTo(map);
+var bufferMocoLayer = L.mapbox.featureLayer().addTo(map);
 var dcBikeLanes = L.mapbox.featureLayer().addTo(map);
+var mocoBikeLanes = L.mapbox.featureLayer().addTo(map);
 
 dcBikeLanes.loadURL('./DC_bikelanes.geojson')
     .on('ready', done);
-
+mocoBikeLanes.loadURL('./Bikeways.geojson')
+    .on('ready', done);
 function done() {
     dcBikeLanes.setStyle({ color: 'green', weight: 2 });
+    mocoBikeLanes.setStyle({ color: 'green', weight: 2 });
 
     function run() {
         var radius = parseInt(document.getElementById('radius').value);
@@ -25,6 +29,14 @@ function done() {
                 "stroke": "#FF69B4",
                 "stroke-width": 2
             });
+        var bufferMoco = turf.buffer(mocoBikeLanes.getGeoJSON(), radius/5280, 'miles');
+        bufferMocoLayer.setGeoJSON(bufferMoco)
+            .setStyle({
+                "fill": "#FF69B4",
+                "stroke": "#FF69B4",
+                "stroke-width": 2
+            });
+
     }
 
     run();
