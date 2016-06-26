da786c771bd91ab7d43da8348a551d9af5cabc24
Alex Weston <alexwweston@gmail.com>
2016-06-26 14:42:52 -0400

    Added @khiggns115's dc buffers and integrated with the layer control

--- site.js
+++ site.js
@@ -3,14 +3,7 @@ L.mapbox.accessToken = 'pk.eyJ1IjoiYWx1bHNoIiwiYSI6ImY0NDBjYTQ1NjU4OGJmMDFiMWQ1Y
 var map = L.mapbox.map('map', 'mapbox.light', { zoomControl: false })
     .setView([38.898, -77.043], 12);
 
-new L.Control.Zoom({ position: 'bottomright' }).addTo(map);
 
-var dcBufferLayer = L.mapbox.featureLayer().addTo(map);
-var dcTrailsBufferLayer = L.mapbox.featureLayer().addTo(map);
-var mocoBufferLayer = L.mapbox.featureLayer().addTo(map);
-var fairfaxBufferLayer = L.mapbox.featureLayer().addTo(map);
-var alexBufferLayer = L.mapbox.featureLayer().addTo(map);
-var arlingtonBufferLayer = L.mapbox.featureLayer().addTo(map);
 
 var dcBikeData = L.mapbox.featureLayer().addTo(map);
 var mocoBikeLanes = L.mapbox.featureLayer().addTo(map);
@@ -18,6 +11,48 @@ var alexBikeTrails = L.mapbox.featureLayer().addTo(map);
 var arlingtonBikeTrails = L.mapbox.featureLayer().addTo(map);
 var fairfaxBikeLanes = L.mapbox.featureLayer().addTo(map);
 
+
+dcBikeData.loadURL('./data/DC_Bike_Paths_all.geojson')
+    .on('ready', bikelane_loaded);
+
+mocoBikeLanes.loadURL('./data/MD_MontgomeryCounty_bike.geojson')
+    .on('ready', bikelane_loaded);
+
+fairfaxBikeLanes.loadURL('./data/VA_FairfaxCounty_Bike.geojson')
+    .on('ready', bikelane_loaded);
+
+alexBikeTrails.loadURL('./data/VA_Alexandria_Bike.geojson')
+    .on('ready', bikelane_loaded);
+
+arlingtonBikeTrails.loadURL('./data/VA_Arlington_Bike.geojson')
+    .on('ready', bikelane_loaded);
+
+// load buffers
+var dcbuffer_500 = L.mapbox.featureLayer().addTo(map);
+dcbuffer_500.loadURL('./buffers/dcBikeLanes_buff_500ft.geojson')
+    .on('ready', buffer_loaded);
+
+var dcbuffer_1000 = L.mapbox.featureLayer();
+dcbuffer_1000.loadURL('./buffers/dcBikeLanes_buff_1000ft.geojson')
+    .on('ready', buffer_loaded);
+
+var dcbuffer_2500 = L.mapbox.featureLayer();
+dcbuffer_2500.loadURL('./buffers/dcBikeLanes_buff_2500ft.geojson')
+    .on('ready', buffer_loaded);
+
+var dcbuffer_5280 = L.mapbox.featureLayer();
+dcbuffer_5280.loadURL('./buffers/dcBikeLanes_buff_5280ft.geojson')
+    .on('ready', buffer_loaded);
+
+// controls
+var overlayMaps = {
+    "500ft": dcbuffer_500,
+    "1000ft": dcbuffer_1000,
+    "2500ft": dcbuffer_2500,
+    "1 mile": dcbuffer_5280
+};
+
+L.control.layers({},overlayMaps).addTo(map);
 var geocoder = L.mapbox.geocoderControl('mapbox.places')
 geocoder.setPosition('topright')
 map.addControl(geocoder);
@@ -34,17 +69,12 @@ geocoder.on('select', function(e){
     search_marker.addTo(map);
 })
 
-dcBikeData.loadURL('./data/DC_Bike_Paths_all.geojson')
-    .on('ready', done);
 
-mocoBikeLanes.loadURL('./data/MD_MontgomeryCounty_bike.geojson')
-    .on('ready', done);
 
-alexBikeTrails.loadURL('./data/VA_Alexandria_Bike.geojson')
-    .on('ready', done);
 
-arlingtonBikeTrails.loadURL('./data/VA_Arlington_Bike.geojson')
-    .on('ready', done);
+
+new L.Control.Zoom({ position: 'bottomright' }).addTo(map);
+
 
 // styles and color paletter for map
 var bikeLaneStyle = { color: 'green', weight: 2 };
@@ -52,6 +82,7 @@ var bufferStyle = { "fill": "#56B6DB",
                     "stroke": "#1A3742",
                     "stroke-width": 2
                 };
+
 // Each buffer feature object needs to have the properties set individually
 function setProperties (buffer) {
     for (var i = 0; i < buffer.features.length; i++) {
@@ -59,23 +90,16 @@ function setProperties (buffer) {
     }
 }
 
-function done(e) {
+// onload callbacks for buffers and bikelanes
+function buffer_loaded(e){
+    var buffer = e.target;
+    setProperties(buffer.getGeoJSON());
+    buffer.setGeoJSON(buffer.getGeoJSON())
+}
+
+function bikelane_loaded(e) {
     var BikeLanes = e.target;
 
     BikeLanes.setStyle(bikeLaneStyle);
 
-    function run() {
-        var radius = parseInt(document.getElementById('radius').value);
-        if (isNaN(radius)) radius = 500;
-
-        var buffer = turf.buffer(BikeLanes.getGeoJSON(), radius / 5280, 'miles');
-        // Each buffer feature object needs to have the properties set individually
-        setProperties(buffer);
-        BikeLanes.setGeoJSON(buffer);
-
-    }
-
-    run();
-
-    document.getElementById('radius').onchange = run;
 }
