e91ad14bb7bc1ba8f5a534b5ed6e8128f6da97c8
Axhon Ruiz <axhonruiz@gmail.com>
2017-02-15 10:53:37 -0500

    added first unit test and unit test npm script

--- test/unit/unit1.unit.spec.js
+++ test/unit/unit1.unit.spec.js
@@ -1,13 +1,10 @@
-const config = require('../nightwatch.conf.js');
-
 
 module.exports = { // adapted from: https://git.io/vodU0
   'Guinea Pig Assert Title': function(browser) {
     browser
-      .url('https://saucelabs.com/test/guinea-pig')
+      .url('localhost:3000')
       .waitForElementVisible('body')
-      .assert.title('I am a page title - Sauce Labs')
-      .saveScreenshot('guinea-pig-test.png')
+      .assert.title('WABA Bike Infrastructure Map Project')
       .end();
   }
 };
